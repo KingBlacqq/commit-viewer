2af30fbfc62373fa5103e0b8fa1409c7aa0cb5ec
KingBlacqq <Leostech26@gmail.com>
2023-12-22 01:05:47 -1200

    updates

--- src/sevices/axios.js
+++ src/sevices/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const githubToken = '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';
+const githubToken = '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';
 const Api = axios.create({
   // baseURL:"https://api.github.com/",
   baseURL:"https://api.github.com/search/repositories?q=language:javaScript+sort:stars&per_page=5",  
   headers: {
     'Accept': 'application/vnd.GitHub.v3+json',
-    'Authorization': 'token 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'
+    'Authorization': 'token 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'
   }
 })
 
